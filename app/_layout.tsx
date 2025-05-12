8ff3117892a5e28637a6483037cca63864efd990
Kediwadenla <wadenlakedi@gmail.com>
2025-05-12 23:23:53 +0700

    add splash screen and lending screen
    lending masih belum selesai

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,15 @@
+import { useFonts } from "expo-font";
 import { Slot } from "expo-router";
 import { AuthProvider } from "../context/AuthContext";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+  });
+
   return (
     <AuthProvider>
       <Slot />
--- app/splash.tsx
+++ app/splash.tsx
@@ -1,65 +1,87 @@
-import { blueBrand, goldBrand } from "@/constants/Colors";
+import { blueBrand } from "@/constants/Colors";
 import { useRouter } from "expo-router";
 import { useEffect } from "react";
-import { ActivityIndicator, Image, StyleSheet, Text, View } from "react-native";
+import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
 import { useAuth } from "../context/AuthContext";
 
+const { width, height } = Dimensions.get("window");
+
 export default function SplashScreen() {
-    const { user, isLoading } = useAuth();
-    const router = useRouter();
+  const { user, isLoading } = useAuth();
+  const router = useRouter();
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            console.log("isLoading:", isLoading);
-            console.log("user:", user);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log("isLoading:", isLoading);
+      console.log("user:", user);
 
-            if (!isLoading) {
-                if (user) {
-                    router.replace("../(tabs)/home");
-                } else {
-                    router.replace("../(auth)/login");
-                }
-            }
-        }, 3000);  // Memberikan delay sedikit lebih lama
+      if (!isLoading) {
+        if (user) {
+          router.replace("../(tabs)/home");
+        } else {
+          router.replace("../(auth)/lending");
+        }
+      }
+    }, 1500); // Memberikan delay sedikit lebih lama
 
-        return () => clearTimeout(timer);
-    }, [isLoading, user]);
+    return () => clearTimeout(timer);
+  }, [isLoading, user]);
 
-    return (
-        <View style={styles.container}>
-            <View style={{ flex: 1 }}>
-                <Image
-                    source={require("@/assets/images/logo.png")}
-                    style={styles.image}
-                    resizeMode="contain"
-                />
-            </View>
-            <View style={{ flex: 1, flexDirection: 'row' }}>
-                <Text style={styles.title1}>konsultasi </Text>
-                <Text style={styles.title2}>hukum</Text>
-            </View>
-            <ActivityIndicator size="large" color="#ffffff" />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={require("@/assets/images/logo.png")} style={styles.image} resizeMode="contain" />
+        <Image source={require("@/assets/images/Tagline.png")} style={styles.tagline} resizeMode="contain" />
+        <Image source={require("@/assets/images/Ilustrasi.png")} style={styles.ilustrasi} resizeMode="contain" />
+      </View>
+
+      <View style={styles.textContainer}>
+        <Text style={{ fontFamily: "Poppins-Light", color: blueBrand }}>dipercaya oleh :</Text>
+      </View>
+
+      <Image source={require("@/assets/images/verifikasi.png")} style={styles.verifikasi} resizeMode="contain" />
+
+      {/* <ActivityIndicator size="large" color="#000000" style={styles.loading} /> */}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#ffffff',
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 20
-    },
-    title1: {
-        color: blueBrand,
-        fontSize: 31
-    },
-    title2: {
-        color: goldBrand,
-        fontSize: 31
-    },
-    image: {
-
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    flex: 1,
+    alignItems: "center",
+  },
+  image: {
+    width: width * 0.35,
+    height: height * 0.06,
+    marginTop: height * 0.07,
+  },
+  tagline: {
+    width: width * 0.6,
+    height: height * 0.12,
+    marginTop: height * 0.09,
+  },
+  ilustrasi: {
+    width: width * 0.7,
+    height: height * 0.25,
+    marginTop: height * 0.07,
+  },
+  verifikasi: {
+    width: width * 0.45,
+    height: height * 0.06,
+    marginTop: height * 0.01,
+    marginBottom: height * 0.09,
+  },
+  textContainer: {
+    alignItems: "center",
+  },
+  loading: {
+    marginTop: height * 0.02,
+  },
 });
